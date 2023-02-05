06eb58d17dd3a233bd58053e96592b98ca5d191e
saurabhsaindhane <85819887+saurabhsaindhane@users.noreply.github.com>
2023-02-05 10:40:30 +0530

    Add files via upload

---
+++ BinaryTreeusingLL/src/Main.java
@@ -0,0 +1,62 @@
+class Node {
+    int data;
+    Node left, right;
+
+    Node(int item) {
+        data = item;
+        left = right = null;
+    }
+}
+
+class BinaryTree {
+    Node root;
+
+    BinaryTree() {
+        root = null;
+    }
+
+    void insert(int data) {
+        root = insertRec(root, data);
+    }
+
+    Node insertRec(Node node, int data) {
+        if (node == null) {
+            node = new Node(data);
+            return node;
+        }
+        if (data < node.data) {
+            node.left = insertRec(node.left, data);
+        } else if (data > node.data) {
+            node.right = insertRec(node.right, data);
+        }
+        return node;
+    }
+
+    void inorder() {
+        inorderRec(root);
+    }
+
+    void inorderRec(Node node) {
+        if (node != null) {
+            inorderRec(node.left);
+            System.out.println(node.data);
+            inorderRec(node.right);
+        }
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        BinaryTree tree = new BinaryTree();
+
+        tree.insert(50);
+        tree.insert(30);
+        tree.insert(20);
+        tree.insert(40);
+        tree.insert(70);
+        tree.insert(60);
+        tree.insert(80);
+
+        tree.inorder();
+    }
+}
